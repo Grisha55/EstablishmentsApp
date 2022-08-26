fe10bae855c743ffa5c77f40f83b3be00e8cb613
Grigory <grishavinyar@icloud.com>
2022-08-26 22:48:40 +0300

    Created the project
    - setuped project with Firebase

---
+++ EstablishmentsApp/EstablishmentsApp/RegistrationViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RegistrationViewController.swift
+//  EstablishmentsApp
+//
+//  Created by Григорий Виняр on 26/08/2022.
+//
+
+import UIKit
+
+class RegistrationViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .green
+  }
+
+
+}
+
